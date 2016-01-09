e8b6716550c41b672c328f6a0429813e362ae00d
Ross Cunning <ross.cunning@gmail.com>
2016-01-08 15:58:48 -1000

    fix accidental removal of Quantity from std curve data

--- steponeR.R
+++ steponeR.R
@@ -43,7 +43,7 @@ steponeR <- function(files=NULL, target.ratios=NULL, fluor.norm=NULL,
   if("STANDARD" %in% tasks) {
     # Process STANDARDS
     std <- data[which(data$Task=="STANDARD"), ]
-    std <- std[, c("CT", names(which(sapply(std, function(x) nlevels(x) > 1))))]
+    std <- std[, c("Quantity", "CT", names(which(sapply(std, function(x) nlevels(x) > 1))))]
     std.lm <- lm(log10(Quantity) ~ ., data=std)
     # Use standard curves to calculate quantities for unknowns
     unk$copies <- 10^predict(std.lm, newdata = unk[, c("CT", "Target.Name", "Filename")])
